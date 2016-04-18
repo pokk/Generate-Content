cd28bb77c698e3a7e5b3ac3e5b1747077e3754b5
Jieyi <pokkbaby@gmail.com>
2016-04-18 21:31:32 +0900

    1. Modify the html connection prototype.

--- content/connection.py
+++ content/connection.py
@@ -10,13 +10,25 @@ class HTMLContentGetter:
     Get the web html source content.
     """
 
-    def __init__(self, url, html_parser=None, decoder='utf8'):
+    def __init__(self, url, decoder='utf8'):
         self._url = url
-        self._parser = html_parser
         self._decoder = decoder
         self._connection = None
 
-    def obtain_origin_content(self):
+    def obtain_content(self, html_parser=None):
+        """
+        Using the html parser parses the content.
+
+        :return: parsed content.
+        """
+
+        if html_parser:
+            html_parser.feed(self.__get_content())
+            return ''.join(html_parser.content)
+        else:
+            return self.__get_content()
+
+    def __get_content(self):
         """
         Get the website content of assignment url.
 
@@ -27,21 +39,6 @@ class HTMLContentGetter:
             return None
 
         return self._connection.read().decode(self._decoder)
-
-    def obtain_parsed_content(self, html_parser=None):
-        """
-        Using the html parser parses the content.
-
-        :return: parsed content.
-        """
-
-        parser = html_parser if html_parser else self._parser
-        if not parser:
-            print("You didn't set a html_parser!!")
-            return None
-
-        parser.feed(self.obtain_origin_content())
-        return ''.join(parser.content)
 
     def __access(self):
         """
@@ -60,7 +57,7 @@ class HTMLContentGetter:
 
 def main():
     getter = HTMLContentGetter('https://en.wikipedia.org/wiki/Markov')
-    print(getter.obtain_parsed_content(WikiArticleParser()))
+    print(getter.obtain_content(WikiArticleParser()))
 
 
 if __name__ == '__main__':
